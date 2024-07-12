34ab26eaef24602298b73a78c4d9ba3a34a8d6c7
coder7475 <robiulhossain7475@gmail.com>
2024-07-13 00:17:30 +0600

    feat: setup a basic navbar struct

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,17 @@ import { Button } from "@/components/ui/button";
 
 const App = () => {
   return (
-    <main className="font-helve">
-      <h1>Hello</h1>
-      <Button>Click me</Button>
+    <main className="font-helve max-w-[1280px] mx-auto mt-3">
+      <nav className="flex justify-between items-center text-base">
+        <h2>accelPro</h2>
+        <ul className="flex justify-between w-[60%] items-center">
+          <li>lorem</li>
+          <li>jipsum</li>
+          <li>sajf</li>
+          <li>dsafdf</li>
+          <Button>Click me</Button>
+        </ul>
+      </nav>
     </main>
   );
 };
